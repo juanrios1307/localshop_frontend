cdb3ba2daf4d2c31bb40130b3979457cd4b8b4af
JuanseHenao <53921087+JuanseHenao@users.noreply.github.com>
2020-10-31 16:45:38 -0500

    Update Login Register and Password change and forgot

---
+++ src/Pages/ForgotPasword.jsx
@@ -0,0 +1,71 @@
+import React,{Component} from 'react';
+import '../assets/css/ForgotPasword.css';
+import Logo from '../assets/images/Logo/BLACK PNG.png';
+import {Link} from 'react-router-dom';
+import Axios from "axios";
+import Swal from "sweetalert2";
+
+class ForgotPasword extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            correo: ''
+        };
+        this.enviarForm = this.enviarForm.bind(this);
+    }
+
+    async enviarForm(e) {
+        e.preventDefault()
+
+        //const url = 'https://peaceful-ridge-86113.herokuapp.com/api/updatepwd'
+        const url = 'http://localhost:5000/api/updatepwd/';
+
+        var config = {
+            method: 'post',
+            url: url,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                "correo": this.state.correo
+            }
+        };
+
+        var response = await Axios(config);
+
+        var data = response.data.data;
+
+        Swal.fire({
+            icon: 'success',
+            title: data
+        })
+
+        localStorage.setItem("correo",this.state.correo)
+    }
+
+    render(){
+        return (
+            <div className="pagforgot">
+                <div className="forgotbox">
+                    <img src={Logo} alt="logo"/>
+                    <div  className="forgottitle">
+                        <h8>¿Olvidaste tu contraseña?</h8>
+                    </div>
+                    <p>
+                        ¡No hay problema! Ingresa tu correo electrónico y te mandaremos las instrucciones para que reestablezcas tu contraseña.
+                    </p>
+                    <form onSubmit={this.enviarForm} >
+                        <input type="email" name="email" placeholder="Correo Electrónico" required
+                               onChange={e => this.setState({correo: e.target.value})}/>
+                        <button>Enviar Link</button>
+                        <div className="loglink">
+                            <Link to="/sing-up">Volver a Iniciar Sesión</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+export default ForgotPasword;
